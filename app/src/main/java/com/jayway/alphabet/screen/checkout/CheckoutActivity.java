b50acd1b88fb10113096a1c07a7775c610ff5aa8
Simon Zettervall <simon.zettervall@jayway.com>
2017-10-14 10:12:07 +0200

    Added a checkout view.

---
+++ app/src/main/java/com/jayway/alphabet/screen/checkout/CheckoutActivity.java
@@ -0,0 +1,18 @@
+package com.jayway.alphabet.screen.checkout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.jayway.alphabet.R;
+
+public class CheckoutActivity extends Activity {
+
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_checkout);
+
+	}
+}
